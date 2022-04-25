cb85c6f85e426d508906cdcd45debb8f17844b36
Sammy Gigi Cantoral Montejo <44757651+sgcm14@users.noreply.github.com>
2022-04-24 22:29:21 -0500

    Create ordenar.component.ts

---
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { Color, Heroe } from 'src/app/ventas/interfaces/ventas.interfaces';
+
+@Component({
+  selector: 'app-ordenar',
+  templateUrl: './ordenar.component.html',
+  styles: [
+  ]
+})
+export class OrdenarComponent {
+
+  enMayusculas = true || false;
+  ordenarPor: string = '';
+
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    },
+  ]
+
+  change() {
+    this.enMayusculas = !this.enMayusculas;
+  }
+
+  orderChange(valor: string) {
+    this.ordenarPor = valor;
+  }
+}
